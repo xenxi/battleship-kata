1ac95d9537e541d87ff176b6e2c5ecafb745866a
xenxi <antoniom.diaz.moreno@gmail.com>
2021-09-12 13:37:27 +0200

    add print reporting feature test

---
+++ batteship_kata_flutter/test/batteship_feature.dart
@@ -0,0 +1,76 @@
+import 'package:batteship_kata_flutter/presentation/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('batteship should', () {
+    testWidgets('print reporting', (WidgetTester tester) async {
+      final aGivenBoard = BoardWidget(
+          carrier: Carrier(x: 3, y: 2),
+          destroyer: Destroyer(x: 8, y: 4),
+          gunship: Gunship(x: 6, y: 4));
+
+      await tester.pumpWidget(aGivenBoard);
+
+      await whenPlayGame(tester);
+
+      expect(find.text('o'), findsNWidgets(15));
+      expect(find.text('X'), findsNWidgets(7));
+      expect(find.text('g'), findsNWidgets(1));
+      expect(find.text('d'), findsNWidgets(2));
+      expect(find.text('c'), findsNWidgets(3));
+      expect(find.text('x'), findsNWidgets(2));
+    });
+  });
+}
+
+Future<void> whenPlayGame(WidgetTester tester) async {
+  final cells = List.generate(
+      10, (index) => List<String>.filled(10, '', growable: false));
+  cells[0][2] = 'o';
+
+  cells[1][1] = 'o';
+  cells[1][3] = 'o';
+
+  cells[2][1] = 'o';
+  cells[2][7] = 'X';
+
+  cells[3][2] = 'X';
+  cells[3][3] = 'X';
+  cells[3][4] = 'X';
+
+  cells[4][1] = 'o';
+  cells[4][6] = 'g';
+  cells[4][8] = 'c';
+
+  cells[5][1] = 'o';
+  cells[5][2] = 'o';
+  cells[5][5] = 'o';
+  cells[5][8] = 'c';
+
+  cells[6][3] = 'o';
+  cells[6][6] = 'o';
+  cells[6][8] = 'c';
+
+  cells[7][1] = 'x';
+  cells[7][5] = 'd';
+  cells[7][8] = 'X';
+
+  cells[8][2] = 'o';
+  cells[8][3] = 'o';
+  cells[8][5] = 'd';
+  cells[8][8] = 'o';
+
+  cells[9][4] = 'o';
+  cells[9][5] = 'x';
+  cells[9][6] = 'o';
+  cells[9][9] = 'X';
+
+  for (var row = 0; row < 10; row++) {
+    for (var col = 0; col < 10; col++) {
+      if (cells[row][col].isNotEmpty) {
+        await tester.tap(find.byKey(Key('x:$col;y:$row')));
+      }
+    }
+  }
+}
