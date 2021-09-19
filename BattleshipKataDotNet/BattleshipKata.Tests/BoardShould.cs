5dc79a442e53f30c36b835786a8a949cee7bab3d
xenxi <antoniom.diaz.moreno@gmail.com>
2021-09-19 12:55:32 +0200

    add test: create_new_board_with_correct_size

---
+++ BattleshipKataDotNet/BattleshipKata.Tests/BoardShould.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BattleshipKata.Tests
+{
+    [TestFixture]
+    public class BoardShould
+    {
+        [Test]
+        public void create_new_board_with_correct_size()
+        {
+            var aGivenHeigh = 10;
+            var aGivenWidth = 15;
+
+            var board = new Board(heigth: aGivenHeigh, width: aGivenWidth);
+
+            board.Height.Should().Be(aGivenHeigh);
+            board.Width.Should().Be(aGivenWidth);
+        }
+    }
+}
