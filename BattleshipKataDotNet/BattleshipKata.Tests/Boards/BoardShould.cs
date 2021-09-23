032884ce782ff4eb50d7407f91e21ec17d08fb89
andiaz <andiaz@aidacanarias.com>
2021-09-23 16:11:47 +0200

    add test red: throw_invalid_coordinates_exception_when_not_all_the_squares_of_a_ship_hit_on_the_board

--- BattleshipKataDotNet/BattleshipKata.Tests/Boards/BoardShould.cs
+++ BattleshipKataDotNet/BattleshipKata.Tests/Boards/BoardShould.cs
@@ -26,7 +26,7 @@ namespace BattleshipKata.Tests.Boards {
             board.Height.Should().Be(HEIGHT);
         }
 
-          [Test]
+        [Test]
         public void place_a_destroyer() {
             var aGivenCoordinates = new Coordinates(0, 0);
             var aGivenDestroyer = new Destroyer(Orientation.Horizontal, aGivenCoordinates);
@@ -39,7 +39,19 @@ namespace BattleshipKata.Tests.Boards {
         [Test]
         public void throw_invalid_coordinates_exception_when_place_ship_outside_board() {
             var aGivenOutSideBoardCoordinates = CoordinatesMother.CreateOutside(board.Size);
-            var aGivenShip = ShipMother.Random(coordinates:aGivenOutSideBoardCoordinates);
+            var aGivenShip = ShipMother.Random(coordinates: aGivenOutSideBoardCoordinates);
+
+            Action action = () => board.PlaceShip(aGivenShip);
+
+            action.Should().Throw<InvalidCoordinates>();
+        }
+
+        [TestCase(9, 10, Orientation.Horizontal)]
+        [TestCase(10, 10, Orientation.Horizontal)]
+        [TestCase(0, 0, Orientation.Vertical)]
+        [TestCase(0, 1, Orientation.Vertical)]
+        public void throw_invalid_coordinates_exception_when_not_all_the_squares_of_a_ship_hit_on_the_board(int x, int y, Orientation orientation) {
+            var aGivenShip = ShipMother.Create(coordinates: CoordinatesMother.Create(x, y), orientation);
 
             Action action = () => board.PlaceShip(aGivenShip);
 
