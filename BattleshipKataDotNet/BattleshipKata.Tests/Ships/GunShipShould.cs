e9ba0e56d05617c4855e132f3249a416b1e00421
xenxi <antoniom.diaz.moreno@gmail.com>
2021-09-26 13:21:31 +0200

    add GunShip tests

--- BattleshipKataDotNet/BattleshipKata.Tests/Ships/GunShipShould.cs
+++ BattleshipKataDotNet/BattleshipKata.Tests/Ships/GunShipShould.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using BattleshipKata.Ships;
+using BattleshipKata.Tests.ValueObjects;
+using BattleshipKata.ValueObjects;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BattleshipKata.Tests.Ships
+{
+    [TestFixture]
+    public class GunShipShould
+    {
+        private GunShip gunShip;
+
+        [Test]
+        public void has_one_coordinates()
+        {
+            gunShip.Coordinates.Count.Should().Be(1);
+        }
+
+        [Test]
+        public void has_one_live()
+        {
+            gunShip.Lives.Should().Be(1);
+        }
+
+        [Test]
+        public void has_type_gun_ship()
+        {
+            gunShip.Type.Should().Be(ShipType.GunShip);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            gunShip = new GunShip(CoordinatesMother.Random());
+        }
+    }
+}
---
+++ BattleshipKataDotNet/BattleshipKata/Ships/GunShip.cs
@@ -0,0 +1,13 @@
+﻿using BattleshipKata.ValueObjects;
+
+namespace BattleshipKata.Ships
+{
+    public class GunShip : Ship
+    {
+        public GunShip(Coordinates position) : base(Orientation.Horizontal, 1, position)
+        {
+        }
+
+        public override ShipType Type => ShipType.GunShip;
+    }
+}
