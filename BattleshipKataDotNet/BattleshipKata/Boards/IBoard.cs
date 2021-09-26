f972969f72436312d3b14bd297b3bfd716cf83b2
xenxi <antoniom.diaz.moreno@gmail.com>
2021-09-26 08:21:13 +0200

    remove unnecesary methods

--- BattleshipKataDotNet/BattleshipKata/Boards/IBoard.cs
+++ BattleshipKataDotNet/BattleshipKata/Boards/IBoard.cs
@@ -5,9 +5,6 @@ namespace BattleshipKata.Boards
 {
     public interface IBoard
     {
-        int Height { get; }
-        int Width { get; }
-
         void PlaceShip(Ship ship);
 
         void Fire(Coordinates expectedCoordinates);
