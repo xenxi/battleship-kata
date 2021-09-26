938991642befc403e6e4ea1c71665db75595e8bb
xenxi <antoniom.diaz.moreno@gmail.com>
2021-09-26 13:15:55 +0200

    add Carrier tests

---
+++ BattleshipKataDotNet/BattleshipKata.Tests/Ships/CarrierShould.cs
@@ -0,0 +1,38 @@
+﻿using BattleshipKata.Ships;
+using BattleshipKata.Tests.ValueObjects;
+using BattleshipKata.ValueObjects;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BattleshipKata.Tests.Ships
+{
+    [TestFixture]
+    public class CarrierShould
+    {
+        private Carrier carrier;
+
+        [Test]
+        public void has_four_coordinates()
+        {
+            carrier.Coordinates.Count.Should().Be(4);
+        }
+
+        [Test]
+        public void has_four_lives()
+        {
+            carrier.Lives.Should().Be(4);
+        }
+
+        [Test]
+        public void has_type_carrier()
+        {
+            carrier.Type.Should().Be(ShipType.Carrier);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            carrier = new Carrier(Orientation.Horizontal, CoordinatesMother.Random());
+        }
+    }
+}
---
+++ BattleshipKataDotNet/BattleshipKata/Ships/Carrier.cs
@@ -0,0 +1,13 @@
+﻿using BattleshipKata.ValueObjects;
+
+namespace BattleshipKata.Ships
+{
+    public class Carrier : Ship
+    {
+        public Carrier(Orientation orientation, Coordinates position) : base(orientation, 4, position)
+        {
+        }
+
+        public override ShipType Type => ShipType.Carrier;
+    }
+}
